79a88e2f7c8fabc13f7de944cc9a2d6d2b706973
engelharten <marcusengelhart@gmail.com>
2016-12-01 23:26:35 +0100

    Day 1 A

--- js/day1.js
+++ js/day1.js
@@ -1,1 +1,41 @@
-console.log('test');
+var data = "R3, L5, R2, L2, R1, L3, R1, R3, L4, R3, L1, L1, R1, L3, R2, L3, L2, R1, R1, L1, R4, L1, L4, R3, L2, L2, R1, L1, R5, R4, R2, L5, L2, R5, R5, L2, R3, R1, R1, L3, R1, L4, L4, L190, L5, L2, R4, L5, R4, R5, L4, R1, R2, L5, R50, L2, R1, R73, R1, L2, R191, R2, L4, R1, L5, L5, R5, L3, L5, L4, R4, R5, L4, R4, R4, R5, L2, L5, R3, L4, L4, L5, R2, R2, R2, R4, L3, R4, R5, L3, R5, L2, R3, L1, R2, R2, L3, L1, R5, L3, L5, R2, R4, R1, L1, L5, R3, R2, L3, L4, L5, L1, R3, L5, L2, R2, L3, L4, L1, R1, R4, R2, R2, R4, R2, R2, L3, L3, L4, R4, L4, L4, R1, L4, L4, R1, L2, R5, R2, R3, R3, L2, L5, R3, L3, R5, L2, R3, R2, L4, L3, L1, R2, L2, L3, L5, R3, L1, L3, L4, L3"
+// data = "R2, L3";
+// data = "R2, R2, R2";
+// data = "R5, L5, R5, R3";
+
+
+var dataArray = data.split(", ");
+
+var xLed = 0;
+var yLed = 0;
+
+var direction = 0;
+
+for (var i = 0; i < dataArray.length; i++) {
+  caclulateSteps(dataArray[i][0], dataArray[i].substring(1));
+}
+
+function caclulateSteps(stepDirection, numberOfSteps) {
+
+    direction = (direction + (stepDirection === 'L' ? -1 : 1) + 4) % 4;
+
+    var numberOfSteps = parseInt(numberOfSteps);
+
+    switch (direction) {
+      case 0: //north
+        yLed += numberOfSteps;
+        break;
+      case 1: //east
+        xLed += numberOfSteps;
+        break;
+      case 2: //south
+        yLed -= numberOfSteps;
+        break;
+      case 3: //west
+        xLed -= numberOfSteps;
+
+    }
+}
+console.log(xLed);
+console.log(yLed);
+console.log('result: ' + (Math.abs(xLed) + Math.abs(yLed)));
